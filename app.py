54dfb1d80b2adc16508842886dd6ad4c6054a191
BISWAJ45 <jit.sonu007@gmail.com>
2025-05-02 01:04:44 +0530

    HTML testing Done

--- app.py
+++ app.py
@@ -108,6 +108,67 @@
 #     app.run(debug=True)
 
 
+# import pickle
+# from flask import Flask, render_template, request, jsonify
+# import numpy as np
+# import pandas as pd
+# from data_preprocessing import FullLoanPreprocessor
+# from data_validation import LoanApplication
+# from pydantic import ValidationError
+
+# app = Flask(__name__)
+# catboost_model = pickle.load(open('catboost_model_version_1.pkl', 'rb'))
+# scalar = pickle.load(open('data_scaling_latest.pkl', 'rb'))
+# preprocessor = pickle.load(open('data_preprocessing_v3.pkl', 'rb'))
+
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
+
+# @app.route('/predict_api', methods=['POST'])
+# def predict_api():
+#     try:
+#         raw_data = request.json['data']
+#         validated_data = LoanApplication(**raw_data).model_dump()
+#     except ValidationError as e:
+#         return jsonify({"error": e.errors()}), 400
+
+#     df = pd.DataFrame([validated_data])
+#     processed_data = preprocessor.transform(df)
+#     new_tran_data = scalar.transform(processed_data)
+#     output = catboost_model.predict(new_tran_data)
+#     prediction = 'Y' if output[0] == 1 else 'N'
+#     return jsonify(prediction=prediction)
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     try:
+#         raw_data = request.form.to_dict()
+#         # Convert all numeric fields manually to float/int
+#         for key in raw_data:
+#             try:
+#                 if '.' in raw_data[key]:
+#                     raw_data[key] = round(float(raw_data[key]), 6)
+#                 else:
+#                     raw_data[key] = int(raw_data[key])
+#             except ValueError:
+#                 pass  # Keep string values as is
+
+#         validated_data = LoanApplication(**raw_data).model_dump()
+#         df = pd.DataFrame([validated_data])
+#         processed_data = preprocessor.transform(df)
+#         new_tran_data = scalar.transform(processed_data)
+#         output = catboost_model.predict(new_tran_data)
+#         prediction = 'Yes' if output[0] == 1 else 'No'
+#         return render_template('home.html', prediction_text=f"The Default Prediction is: {prediction}")
+
+#     except ValidationError as e:
+#         return render_template('home.html', prediction_text="Invalid input data", error=e.errors())
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
 import pickle
 from flask import Flask, render_template, request, jsonify
 import numpy as np
@@ -117,54 +178,42 @@ from data_validation import LoanApplication
 from pydantic import ValidationError
 
 app = Flask(__name__)
+
 catboost_model = pickle.load(open('catboost_model_version_1.pkl', 'rb'))
 scalar = pickle.load(open('data_scaling_latest.pkl', 'rb'))
 preprocessor = pickle.load(open('data_preprocessing_v3.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-
-@app.route('/predict_api', methods=['POST'])
-def predict_api():
-    try:
-        raw_data = request.json['data']
-        validated_data = LoanApplication(**raw_data).model_dump()
-    except ValidationError as e:
-        return jsonify({"error": e.errors()}), 400
-
-    df = pd.DataFrame([validated_data])
-    processed_data = preprocessor.transform(df)
-    new_tran_data = scalar.transform(processed_data)
-    output = catboost_model.predict(new_tran_data)
-    prediction = 'Y' if output[0] == 1 else 'N'
-    return jsonify(prediction=prediction)
+    return render_template('home.html', form_data={}, prediction_text=None, error=None)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        raw_data = request.form.to_dict()
-        # Convert all numeric fields manually to float/int
-        for key in raw_data:
-            try:
-                if '.' in raw_data[key]:
-                    raw_data[key] = round(float(raw_data[key]), 6)
-                else:
-                    raw_data[key] = int(raw_data[key])
-            except ValueError:
-                pass  # Keep string values as is
+    raw_data = request.form.to_dict()
 
+    for key in raw_data:
+        try:
+            if '.' in raw_data[key]:
+                raw_data[key] = round(float(raw_data[key]), 6)
+            else:
+                raw_data[key] = int(raw_data[key])
+        except ValueError:
+            pass  # Leave as string if not a number
+
+    try:
         validated_data = LoanApplication(**raw_data).model_dump()
         df = pd.DataFrame([validated_data])
         processed_data = preprocessor.transform(df)
         new_tran_data = scalar.transform(processed_data)
         output = catboost_model.predict(new_tran_data)
         prediction = 'Yes' if output[0] == 1 else 'No'
-        return render_template('home.html', prediction_text=f"The Default Prediction is: {prediction}")
+
+        return render_template('home.html', prediction_text=f"The Default Prediction is: {prediction}", form_data=request.form, error=None)
 
     except ValidationError as e:
-        return render_template('home.html', prediction_text="Invalid input data", error=e.errors())
+        return render_template('home.html', prediction_text="Invalid input data", form_data=request.form, error=e.errors())
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
